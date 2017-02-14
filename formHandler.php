fa31cf1c93998526295e5a1ca6e3481bbd91c1c9
brianahart <bnh5et@virginia.edu>
2017-02-14 14:45:37 -0500

    Checks for empty fields

--- formHandler.php
+++ formHandler.php
@@ -14,62 +14,10 @@
       <td align="center"><strong>Parameter</strong></td>
       <td align="center"><string>Value</string></td>
     </tr>
-    <tr>
+    <!--<tr>
       <td width="20%">MAILTO</td> 
       <td><?php echo $_POST['MAILTO']?></td>      
-    </tr>
-    <tr>
-      <td width="20%">FName</td> 
-      <td><?php echo $_POST['FName']?></td>      
-    </tr>
-    <tr>
-      <td width="20%">LName</td>
-      <td><?php echo $_POST['LName']?></td>      
-    </tr>
-    <tr>
-      <td width="20%">Major</td>
-      <td><?php echo $_POST['Major']?></td>      
-    </tr>
-    <tr>
-      <td width="20%">OtherMajor</td>
-      <td><?php echo $_POST['OtherMajor']?></td>      
-    </tr>
-    <tr>
-      <td width="20%">EmailAddress</td>
-      <td><?php echo $_POST['EmailAddress']?></td>      
-    </tr>
-    <tr>
-      <td width="20%">Phone</td>
-      <td><?php echo $_POST['Phone']?></td>      
-    </tr>
-    <tr>
-      <td width="20%">WebSiteURL</td>
-      <td><?php echo $_POST['WebSiteURL']?></td>      
-    </tr>
-    <tr>
-      <td width="20%">LevelHTML</td>
-      <td><?php echo $_POST['LevelHTML']?></td>      
-    </tr>
-    <tr>
-      <td width="20%">LevelJava</td>
-      <td><?php echo $_POST['LevelJava']?></td>      
-    </tr>
-    <tr>
-      <td width="20%">LevelJS</td>
-      <td><?php echo $_POST['LevelJS']?></td>      
-    </tr>
-    <tr>
-      <td width="20%">LevelServlets</td>
-      <td><?php echo $_POST['LevelServlets']?></td>      
-    </tr>
-    <tr>
-      <td width="20%">LevelJSP</td>
-      <td><?php echo $_POST['LevelJSP']?></td>      
-    </tr>
-    <tr>
-      <td width="20%">NewOrUpdate</td>
-      <td><?php echo $_POST['NewOrUpdate']?></td>      
-    </tr>
+    </tr>-->
     <tr>
       <td width="20%">submit</td>
       <td><?php echo $_POST['submit']?></td>      
