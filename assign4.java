a1510a7ff0e1ceeeed82dee964c9e904da236320
pitchersam <pitchersam@gmail.com>
2017-03-17 13:30:27 -0400

    Update assign4.java

--- assign4.java
+++ assign4.java
@@ -30,6 +30,9 @@ public class assign4 extends HttpServlet {
 
 	ArrayList<String> questions = new ArrayList<String>();
 	ArrayList<String> answers = new ArrayList<String>();
+	ArrayList<Integer> row = new ArrayList<Integer>();
+	ArrayList<Integer> column = new ArrayList<Integer>();
+	ArrayList<Integer> score = new ArrayList<Integer>();
     
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -55,9 +58,10 @@ public class assign4 extends HttpServlet {
 		Scanner reader = new Scanner(url.openStream());
 		//account for there existing both questions and answers
 		count = count / 2;
-		//add the questions and answers to question and anser arraylists
+		//add the questions and answers to question and answer ArrayLists
 		for (int i = 0; i < count; i++)
 		{
+			//if (!reader.nextLine().equals("Question: "))
 			questions.add(reader.nextLine());
 			answers.add(reader.nextLine());
 		}
@@ -72,7 +76,6 @@ public class assign4 extends HttpServlet {
 			//button that goes back to PHP data entry form from assignment 3 on the CSLAB server
 			//"Create Game button"
 		
-		
 		response.getWriter().append("Served at: ").append(request.getContextPath());	
 	}
 
@@ -82,12 +85,10 @@ public class assign4 extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//this is for the submit button
 		
-		
-		try {
-		
-		
+		try 
+		{
 			PrintWriter writer = new PrintWriter("submission.txt", "UTF-8");
-			//dummy strings for question, answer, row, column, score
+			// TODO dummy strings for question, answer, row, column, score
 			writer.println("Question" + "; " + "Answer" + "; " + "row" + "; " + "column" + "; " + "score" );
 			writer.close();
 		} 
@@ -95,13 +96,12 @@ public class assign4 extends HttpServlet {
 		{
 			System.out.println("Could not write to file");
 		}
+		
 		//process form data submission
 	    //take the list of questions, answers, rows, columns, scores
 		//write these to a text file on your machine
 			//can use comma or semicolon to separate each piece of data
 		//http://www.cs.virginia.edu/~up3f/cs4640/inclass/simpleform.java
-		
-		
 		
 		//create jeopardy game and display it on screen
 			//include button that allows user to go back to edit the question
@@ -109,21 +109,54 @@ public class assign4 extends HttpServlet {
 			//can do a formhandler:  http://www.cs.virginia.edu/~up3f/cs4640/examples/servlet/formHandler.java
 		try 
 		{
+			
+			//figure out how many rows and columns we need
+			int maxrows = 0;
+			int maxcols = 0;
+			for (int i = 0; i < row.size(); i++)
+			{
+				if (row.get(i) > maxrows)
+				{
+					maxrows = row.get(i);
+				}
+			}
+			
+			for (int i = 0; i < column.size(); i++)
+			{
+				if (column.get(i) > maxcols)
+				{
+					maxcols = column.get(i);
+				}
+			}
+			
+			
+			
 			response.setContentType("text/html");
 			PrintWriter jeopardyWriter = response.getWriter();
 		
 			String str;
 			Enumeration input = request.getParameterNames();
 		
+			jeopardyWriter.println("<html>");
+			//TODO and so on, to create rest of jeopardy grid
+			
 			while (input.hasMoreElements())
 			{
 				str = (String) input.nextElement();
 				if (!str.equalsIgnoreCase("submit"))
 				{
-					//add the things from input
+					for (int i = 0; i < maxrows; i++)
+					{
+						//print right number of rows
+						//jeopardyWriter.println("<tr>");
+						//then, print right number of columns and add score
+					}
+					//add the things from input to the grid, only if it's not submit
+					//we want the score in the grid, that is all
 				}
 			}
-		
+			//closing html tags to jeopardyWriter
+			jeopardyWriter.println("</html>");
 			jeopardyWriter.close();
 		} 
 		catch (IOException e) 
