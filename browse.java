0a7ae67f74be0b83598df4c22de23590ccf314ec
Briana Hart <brianahart95@gmail.com>
2017-04-25 15:20:24 -0400

    Update browse.java

--- browse.java
+++ browse.java
@@ -21,10 +21,11 @@ public class browse extends HttpServlet {
 	private static String LogoutServlet = "http://localhost:8080/Jeopardy/logout";
 	private static String CreateGridServlet = "http://localhost:8080/Jeopardy/createGrid";
 	private static String BrowseServlet = "http://localhost:8080/Jeopardy/browse";
+	private static String StartGameJSP = "http://localhost:8080/Jeopardy/startGame.jsp";
 
 	// doPost() tells doGet() when the login is invalid.
 	private static boolean invalidID = false;
-	
+
 	private static String btn;
 
 	private String user;
@@ -49,28 +50,35 @@ public class browse extends HttpServlet {
 		HttpSession session = request.getSession(true);
 		
 		btn = request.getParameter("btn");
-		
-		session.setAttribute("updated", false);
-		
+
+		//session.setAttribute("updated", false);
+
 		int gameNum;
-		
+		Map<String, String> gameList = getGames();
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");  
+
 		if (btn.contains("Delete")) {
-			btn = btn.replace("Delete Game ","");
+			btn = btn.replace("Delete Game ", "");
 			gameNum = Integer.parseInt(btn);
-			deleteGame(gameNum);
+
+			if (gameList.get(String.valueOf(gameNum)).equals(user)) {
+				System.out.println("They match");
+				deleteGame(gameNum);
+			}
+
+		} else if (btn.contains("Update")) {
+			btn = btn.replace("Update Game ", "");
+			gameNum = Integer.parseInt(btn);
+			if (gameList.get(String.valueOf(gameNum)).equals(user)) {
+				session.setAttribute("GameNum", gameNum);
+				session.setAttribute("updated", true);
+				response.sendRedirect(CreateGridServlet);
+			}
 		}
-		else if(btn.contains("Update")) {
-			System.out.println("Update clicked");
-			btn = btn.replace("Update Game ","");
-			gameNum = Integer.parseInt(btn);			
-			session.setAttribute("GameNum", gameNum);
-			response.sendRedirect(CreateGridServlet);
-			session.setAttribute("updated", true);
-		}	
-		
 		doGet(request, response);
-		
 	}
+
 	public void PrintHead(PrintWriter out) {
 		out.println("<!DOCTYPE html>");
 		out.println("<html>");
@@ -159,39 +167,42 @@ public class browse extends HttpServlet {
 		out.println("			 	<th></th>");
 		out.println("			 	<th></th>");
 		out.println("			 </tr>");
-		
+
 		Map<String, String> gameList = getGames();
-		
+
 		Set<String> keys = gameList.keySet();
-		
-		for(String s : keys){
+
+		for (String s : keys) {
 			out.println("			 <tr>");
 			out.println("			 	<td>" + s + "</td>");
 			out.println("			 	<td>" + gameList.get(s) + "</td>");
 			out.println("			 	<td align=\"center\">");
 			out.println("			 		<form action=\"" + BrowseServlet + "\" method=\"post\">");
-			out.println("			 			<input class=\"form\" type=\"submit\" value=\"Update Game " + s + "\" name=\"btn\"> ");
+			out.println("			 			<input class=\"form\" type=\"submit\" value=\"Update Game " + s
+					+ "\" name=\"btn\"> ");
 			out.println("			 		</form>");
 			out.println("			 	</td>");
 			out.println("			 	<td align=\"center\">");
 			out.println("			 		<form action=\"" + BrowseServlet + "\" method=\"post\">");
-			out.println("			 			<input class=\"form\" type=\"submit\" value=\"Delete Game " + s + "\" name=\"btn\">");
+			out.println("			 			<input class=\"form\" type=\"submit\" value=\"Delete Game " + s
+					+ "\" name=\"btn\">");
 			out.println("			 		</form>");
 			out.println("			 	</td>");
 			out.println("			 	<td align=\"center\">");
 			out.println("			 		<form>");
-			out.println("			 			<input class=\"form\" type=\"submit\" value=\"Play Game " + s + "\" name=\"btn\">");
+			out.println("			 			<input class=\"form\" type=\"submit\" value=\"Play Game " + s
+					+ "\" name=\"btn\">");
 			out.println("			 		</form>");
 			out.println("			 	</td>");
 			out.println("			 </tr>");
 		}
-		
+
 		out.println("		</table>");
 		out.println("	</div>");
 		out.println("</body>");
 		out.println("</html>");
 	}
-	
+
 	public Map<String, String> getGames() {
 		File file = new File("/Users/brianahart/Documents/submission.txt");
 		Scanner sc;
@@ -199,63 +210,62 @@ public class browse extends HttpServlet {
 
 		try {
 			sc = new Scanner(file);
-			
+
 			while (sc.hasNext()) {
 				String nl = sc.nextLine();
 				String[] parsedLine = nl.split(";");
-							
-				if(!games.containsKey(parsedLine[0])){
+
+				if (!games.containsKey(parsedLine[0])) {
 					games.put(parsedLine[0], parsedLine[1]);
 				}
-			}	
-			
+			}
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		return games;
 	}
 
-	
 	public void deleteGame(int gameNum) {
 
 		try {
 			File file = new File("/Users/brianahart/Documents/submission.txt");
 			Scanner sc = new Scanner(file);
-			String nl = "";	
+			String nl = "";
 			int count = 0;
-			
-			while(sc.hasNext()) {
+
+			while (sc.hasNext()) {
 				nl = sc.nextLine();
-				
-				if(nl.contains(";")){
+
+				if (nl.contains(";")) {
 					count++;
 				}
 			}
 			sc.close();
-			
-			String[] newFile = new String[count];			
-			Scanner scanner = new Scanner(file);			
-			
-			for(int i = 0; i < count; i++){
+
+			String[] newFile = new String[count];
+			Scanner scanner = new Scanner(file);
+
+			for (int i = 0; i < count; i++) {
 				nl = scanner.nextLine();
 				String[] parsedLine = nl.split(";");
-				
-				if(Integer.parseInt(parsedLine[0]) != gameNum) {
+
+				if (Integer.parseInt(parsedLine[0]) != gameNum) {
 					newFile[i] = nl;
 				}
-			}		
+			}
 			scanner.close();
 
 			FileWriter fw = new FileWriter("/Users/brianahart/Documents/submission.txt");
-			
-			for (int i = 0; i < count; i++){
-				if(newFile[i] != null){
+
+			for (int i = 0; i < count; i++) {
+				if (newFile[i] != null) {
 					fw.write(newFile[i]);
 					fw.write("\n");
 				}
-			}		
+			}
 			fw.close();
-			
+
 		} catch (IOException e) {
 			System.out.println("Could not write to file");
 		}
